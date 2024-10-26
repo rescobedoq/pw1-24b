7e78909b40b00ec420a60a6c39eb390db9ba76ff
Richart Escobedo Quispe <rescobedoq@unsa.edu.pe>
2024-10-25 23:19:59 -0500

    ejercicios Perl sobre expresiones regulares

--- trab03/test/env.pl
+++ trab03/test/env.pl
@@ -1,1 +1,8 @@
-/usr/lib/cgi-bin/getpost/env.pl
+#!/usr/bin/perl -w
+
+print "Content-type:text/html\r\n\r\n";
+
+#print $ENV{'HTTP_USER_AGENT'};
+print $ENV{'QUERY_STRING'};
+
+#https://perlenespanol.com/tutoriales/html/variables_de_entorno.html
--- trab03/test/matchExtracting.pl
+++ trab03/test/matchExtracting.pl
@@ -1,1 +1,15 @@
-/usr/lib/cgi-bin/getpost/matchExtracting.pl
+#!/usr/bin/perl
+
+$time = "09:40:17";
+
+# extract hours, minutes, seconds
+$time =~ /(\d\d):(\d\d):(\d\d)/;  # match hh:mm:ss format
+
+print $hours = $1;
+print "\n";
+print $minutes = $2;
+print "\n";
+print $seconds = $3;
+print "\n";
+
+#https://perldoc.perl.org/perlrequick
